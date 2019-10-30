99bd657a67d3a9fe0e1c8f35c95fe5b7ced7808a
Administrador <>
2019-10-30 11:40:27 -0500

    - Cambio remoto

--- src/main/java/pe/edu/idat/daa2/gitnetbeans/App.java
+++ src/main/java/pe/edu/idat/daa2/gitnetbeans/App.java
@@ -3,6 +3,6 @@ package pe.edu.idat.daa2.gitnetbeans;
 
 public class App {
     public static void main (String[]args){
-        System.out.println("HOLA");
+        System.out.println("HOLA GITHUB");
     }
 }
